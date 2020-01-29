b55ecdd6a15e1c851c93afc32719f2788c1398db
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2020-01-29 22:13:12 +0100

    PyPi packaging

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+    name = 'shinden',
+    packages = ['shinden'],  
+    version = '0.3',      
+    license='MIT',        
+    description = 'bs4 web scrapping api for shinden.pl',   
+    author = 'ShaderLight',                    
+    url = 'https://github.com/ShaderLight/shinden.py',   
+    download_url = '',   
+    keywords = ['shinden', 'api', 'web scrapping'],   
+    install_requires=[            
+            'requests',
+            'beautifulsoup4',
+        ],
+    classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    ],
+)
