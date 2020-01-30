ea139a00e012ed1cec847d9bd566dd1f0856a945
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2020-01-30 15:54:53 +0100

    PyPi README fix

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-from distutils.core import setup
-setup(
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
     name = 'shinden',
     packages = ['shinden'],  
-    version = '0.3.3',      
+    version = '0.3.4',      
     license='MIT',        
-    description = 'bs4 web scrapping api for shinden.pl',   
+    description = 'bs4 web scrapping api for shinden.pl',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
     author = 'ShaderLight',                    
     url = 'https://github.com/ShaderLight/shinden.py',   
-    download_url = 'https://github.com/ShaderLight/shinden.py/archive/v0.3.3.tar.gz',   
+    download_url = 'https://github.com/ShaderLight/shinden.py/archive/v0.3.4.tar.gz',   
     keywords = ['shinden', 'api', 'web scrapping'],   
     install_requires=[            
             'requests',
