277c01a8b9e1eef4bca592aecbc5d008383298ae
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2020-04-11 17:49:01 +0200

    More tests

--- tests/shinden_test.py
+++ tests/shinden_test.py
@@ -1,25 +1,40 @@
 import pytest
-from shinden.scrapping import*
+import shinden.scrapping as sh
 
 def test_search_titles():
-        results = search_titles('alchemist')
+        results = sh.search_titles('alchemist')
         first_result = results[0]
         assert first_result.title == 'Fullmetal Alchemist'
 
 def test_top_ten_titles():
-        results = get_top_ten_titles()
+        results = sh.get_top_ten_titles()
         assert len(results) == 10
 
 def test_character():
-        results = search_characters('Violet Evergarden', 'contains', False)
+        results = sh.search_characters('Violet Evergarden', 'contains', False)
         first_result = results[0]
         assert first_result.name == 'Violet Evergarden'
 
 def test_tags():
-        results = get_tags()
+        results = sh.get_tags()
         assert results != []
 
 def test_user():
-        results = search_users('Shaderlight')
+        results = sh.search_users('Shaderlight')
         first_result = results[0]
         assert first_result.nickname == 'Shaderlight'
+
+def test_search_titles_without_result():
+        results = sh.search_titles('jkfbsjhdfbwkefbdv wehfdfwejidhnjk')
+        print(results)
+        assert results == None
+
+def test_search_character_without_result():
+        results = sh.search_titles('jkfbsjhdfbwkefbdv wehfdfwejidhnjk')
+        print(results)
+        assert results == None
+
+def test_user_without_result():
+        results = sh.search_titles('jkfbsjhdfbwkefbdv wehfdfwejidhnjk')
+        print(results)
+        assert results == None
